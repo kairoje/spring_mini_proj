5fccffec6596788cc3bf667bde7bae4967f0010f
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 15:54:27 -0700

    Book list optional added to genre model

--- library /src/main/java/com/libraries/model/GenreModel.java
+++ library /src/main/java/com/libraries/model/GenreModel.java
@@ -1,8 +1,13 @@
 package com.libraries.model;
 
 import lombok.*;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.awt.print.Book;
+import java.util.List;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -23,4 +28,13 @@ public class GenreModel {
 
     @Column
     private String description;
+
+    @OneToMany(mappedBy = "genre", orphanRemoval = true)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<BookModel> bookList;
+
+    public Optional<BookModel> findBookById(Long bookId){
+        return bookList.stream().filter((book)->book.getId().equals(bookId)).findFirst();
+
+    }
 }
