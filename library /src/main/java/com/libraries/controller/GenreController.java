21a7abc9dd9f914fbac309db1194b3b8cad4c882
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 12:16:48 -0700

    Mapping added to controller for get genre endpoint

--- library /src/main/java/com/libraries/controller/GenreController.java
+++ library /src/main/java/com/libraries/controller/GenreController.java
@@ -2,11 +2,14 @@ package com.libraries.controller;
 
 import com.libraries.model.GenreModel;
 import com.libraries.service.GenreService;
+import org.ietf.jgss.GSSName;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -20,5 +23,10 @@ public class GenreController {
 
     @GetMapping(path = "/genres/") // => http://localhost:9096/api/genres/
     public List<GenreModel> getGenres() { return genreService.getGenres(); }
+
+    @GetMapping(path = "/genres/{genreId}/") // => http://localhost:9096/api/genres/1/
+    public Optional<GenreModel> getGenre(@PathVariable(value = "genreId") Long genreId){
+        return genreService.getGenre(genreId);
+    }
 
 }
