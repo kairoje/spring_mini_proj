2eb70184a04d4f81d6f45095ecb7416cf222aca6
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 19:29:40 -0700

    Configuring user details

---
+++ library /src/main/java/com/libraries/security/MyUserDetails.java
@@ -0,0 +1,57 @@
+package com.libraries.security;
+
+import com.libraries.model.UserModel;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class MyUserDetails extends UserModel {
+
+    private final UserModel user;
+
+    public MyUserDetails(UserModel user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // used for verification
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+}
