746c0fa9170122e516977eb549d76685fd6ed5a0
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 19:48:39 -0700

    Adding create user login request authentication

--- library /src/main/java/com/libraries/service/UserService.java
+++ library /src/main/java/com/libraries/service/UserService.java
@@ -2,13 +2,20 @@ package com.libraries.service;
 
 import com.libraries.exceptions.InformationExistException;
 import com.libraries.model.UserModel;
+import com.libraries.model.request.LoginRequest;
 import com.libraries.repository.UserRepository;
 import com.libraries.security.JWTUtils;
+import com.libraries.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -36,6 +43,19 @@ public class UserService {
             return userRepository.save(userObject);
         } else {
             throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " already exists");
+        }
+    }
+
+    public Optional<String> loginUser(LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try{
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e){
+            return Optional.empty();
         }
     }
 
