d5dc2524228929e1c08bd8a7c3c727e6553c04f9
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-20 01:15:39 -0700

    Javadoc for genre repository finding genres

--- library /src/main/java/com/libraries/repository/GenreRepository.java
+++ library /src/main/java/com/libraries/repository/GenreRepository.java
@@ -10,10 +10,26 @@ import java.util.List;
 @Repository
 public interface GenreRepository extends JpaRepository<GenreModel, String>{
 
+    /**
+     * For finding genre by name
+     * @param genreName
+     *
+     */
     GenreModel findByName(String genreName);
 
+    /**
+     * Find genre by genre id and user id
+     * @param categoryId
+     * @param userId
+     *
+     */
     GenreModel findByIdAndUserId(Long categoryId, Long userId);
 
+    /**
+     * Find the list of user's genres with the user id
+     * @param id
+     *
+     */
     List<GenreModel> findByUserId(Long id);
 
 }
