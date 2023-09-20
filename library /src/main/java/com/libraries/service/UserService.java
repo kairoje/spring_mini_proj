84af48fad91e50e95fdb07d568f6a28e5c6fd885
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 19:45:58 -0700

    Adding constructor for user service authentication

--- library /src/main/java/com/libraries/service/UserService.java
+++ library /src/main/java/com/libraries/service/UserService.java
@@ -3,6 +3,8 @@ package com.libraries.service;
 import com.libraries.model.UserModel;
 import com.libraries.repository.UserRepository;
 import com.libraries.security.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,18 @@ public class UserService {
 
     private final JWTUtils jwtUtils;
     private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils,
+                       @Lazy AuthenticationManager authenticationManager){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
+
+
 
     public UserModel findUserByEmailAddress(String emailAddress) {
         return userRepository.findUserByEmailAddress(emailAddress);
