1663c49623009bd25fb1f4c80d86489feabce40c
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-20 01:03:22 -0700

    Javadoc for informaiton not found exceptions

--- library /src/main/java/com/libraries/exceptions/InformationNotFoundException.java
+++ library /src/main/java/com/libraries/exceptions/InformationNotFoundException.java
@@ -3,6 +3,9 @@ package com.libraries.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * @returns a message describing that the entered data can not be found
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
     public InformationNotFoundException(String message) { super(message); }
