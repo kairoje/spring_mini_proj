3155f7cabadf72d092e4bdf07defca15465a504d
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-19 18:52:13 -0700

    Creating a user service class

---
+++ library /src/main/java/com/libraries/service/UserService.java
@@ -0,0 +1,10 @@
+package com.libraries.service;
+
+import com.libraries.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
