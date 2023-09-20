3946de757d78413e21d2645d41c881a731f39f69
kairoje <129036688+kairoje@users.noreply.github.com>
2023-09-20 01:01:46 -0700

    Javadoc for informationExist exception

--- library /src/main/java/com/libraries/exceptions/InformationExistException.java
+++ library /src/main/java/com/libraries/exceptions/InformationExistException.java
@@ -5,5 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
+
+    /**
+     * @returns a message describing that the entered data does not exist
+     */
     public InformationExistException(String message) { super(message); }
 }
